6414a24ef2800a64feae4633a63110a3988c2c5c
Luiz Felipe <luiz.felipe.neves3@gmail.com>
2020-09-12 18:13:43 -0300

    first commit

---
+++ src/Http/Controllers/FormularioContatoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Felipe\FormularioContato\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//use Felipe\FormularioContato\Model\FormularioContato;
+
+class FormularioContatoController extends Controller {
+
+    public function index()
+    {
+        return view('formulario-contato::contato');
+    }
+
+    public function enviarEmail()
+    {
+
+    }
+}
